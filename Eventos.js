672a2366110e0d40b494c1d61e83a0e39b361e81
Brian Morales <briansmor09@gmail.com>
2018-01-15 18:23:40 -0600

    Agregando switch

--- Eventos.js
+++ Eventos.js
@@ -1,1 +1,24 @@
+var teclas ={
+  LEFT: 37,
+  UP: 38,
+  RIGHT: 39,
+  DOWN: 40
+};
 
+document.addEventListener("keydown", dibujarTeclado);
+function dibujarTeclado(evento){
+    switch(evento.keyCode){
+      case teclas.UP:
+      break;
+      case teclas.DOWN:
+      break;
+      case teclas.LEFT:
+      break;
+      case teclas.RIGHT:
+      break;
+      default:
+      break;
+    }
+
+
+}
