d9dc1a478babcde740ccfbbe76c64222b1541441
Brian Morales <briansmor09@gmail.com>
2018-01-17 19:43:42 -0600

    Arreglando errores

--- Eventos.js
+++ Eventos.js
@@ -1,40 +1,50 @@
-var teclas ={
-  LEFT: 37,
+var teclas = {
   UP: 38,
-  RIGHT: 39,
-  DOWN: 40
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39
 };
 
 document.addEventListener("keydown", dibujarTeclado);
-var cuadrito= document.getElementById("Area");
-var lienzo=cuadrito.getContext("2d");
-var x=150;
-var y=150;
+var cuadrito = document.getElementById("area_de_dibujo");
+var papel = cuadrito.getContext("2d");
+var x = 150;
+var y = 150;
 
+dibujarLinea("red", x-1, y-1, x+1, y+1, papel);
 
-
-function dibujarline(color,xini,xfin,yini,yfin){
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal, lienzo)
+{
   lienzo.beginPath();
-  lienzo.strokeStyle=color;
-  lienzo.moveTo(xini,yini);
-  lienzo.lineTo(xfin,yfin);
+  lienzo.strokeStyle = color;
+  lienzo.lineWidth = 3;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
   lienzo.stroke();
   lienzo.closePath();
 }
-dibujarline("red",50,50,20,20);
-function dibujarTeclado(evento){
-    switch(evento.keyCode){
-      case teclas.UP:
-      break;
-      case teclas.DOWN:
-      break;
-      case teclas.LEFT:
-      break;
-      case teclas.RIGHT:
-      break;
-      default:
-      break;
-    }
 
-
+function dibujarTeclado(evento)
+{
+  var colorcito = "#FAA";
+  var movimiento = 5;
+  switch(evento.keyCode)
+  {
+    case teclas.UP:
+      dibujarLinea(colorcito, x, y, x, y - movimiento, papel);
+      y = y - movimiento;
+    break;
+    case teclas.DOWN:
+      dibujarLinea(colorcito, x, y, x, y + movimiento, papel);
+      y = y + movimiento;
+    break;
+    case teclas.LEFT:
+      dibujarLinea(colorcito, x, y, x - movimiento, y, papel);
+      x = x - movimiento;
+    break;
+    case teclas.RIGHT:
+      dibujarLinea(colorcito, x, y, x + movimiento, y, papel);
+      x = x + movimiento;
+    break;
+  }
 }
